fd9424c101c2a1d961f46dd3bc4f4bea2540d1c4
ctjsctjs <ctjsctjs@gmail.com>
2018-03-15 15:00:24 +0800

    Fixed webcam script, added function for checkin/out scan success

--- public/js/qrScanner.js
+++ public/js/qrScanner.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {  
+$( document ).ready(function() {
   let scanner = new Instascan.Scanner({ video: document.getElementById('qrVideo') });
   scanner.addListener('scan', function (content) {
     /*
@@ -29,10 +29,32 @@ function onQRScan(content){
       },
       dataType: "json",
       success: function(data) {
-          
+
       },
       error: function(xhr, status){
           console.log("AJAX ERROR GETTING COURSES: " + xhr.status);
       }
   });
+}
+
+//Outputcheck in message and time, refresh page after timer countdown
+function checkinTime(time){
+  //Replace html content to show time
+  $("#qrVideo").hide();
+  $(".popup-text").html("Check in Success!")
+  $(".popup-smalltext").html("You have successfully checked in at " + time);
+
+  //Timeout after 5 seconds torefresh page
+  setTimeout(function(){ window.location = "/"; }, 5000);
+}
+
+//Outputcheck out message and time, refresh page after timer countdown
+function checkoutTime(time){
+  //Replace html content to show time
+  $("#qrVideo").hide();
+  $(".popup-text").html("Check out Success!")
+  $(".popup-smalltext").html("You have successfully checked out at " + time)
+
+  //Timeout after 5 seconds torefresh page
+  setTimeout(function(){ window.location = "/"; }, 5000);
 }
