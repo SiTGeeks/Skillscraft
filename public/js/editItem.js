72af1b846950891cbad311b0bcea6fb11ce68f8f
ctjsctjs <ctjsctjs@gmail.com>
2018-03-23 15:13:30 +0800

    Updated level input to selector dropdown

--- public/js/editItem.js
+++ public/js/editItem.js
@@ -20,6 +20,15 @@
 //   generateItem('Clement', '97577347', 'ctjsctjs@gmail.com');
 // });
 
+//DEMO
+list = ["DOG", "CAT"];
+generateOptions(list);
+
+//Init date and time picker
+$( function() {
+  $("#datepicker").datepicker();
+  $('#timepicker').timepicker();
+} );
 
 function deleteRegistration(workshopId,name,contact,email,removeBtn){
   var registration = {
@@ -42,5 +51,14 @@ function deleteRegistration(workshopId,name,contact,email,removeBtn){
     error: function(xhr, status){
         console.log("AJAX ERROR GETTING COURSES: " + xhr.status);
     }
+  });
+}
+
+function generateOptions(list){
+  $.each(list, function (i, item) {
+    $('#input-level').append($('<option>', {
+      value: item,
+      text : item
+    }));
   });
 }
