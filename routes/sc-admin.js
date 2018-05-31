652502d5512e2e54ecf4714a3d7c4966f0ecde97
Edwin Wong <edwinwjs94school@gmail.com>
2018-06-01 00:49:03 +0800

    ew

--- routes/sc-admin.js
+++ routes/sc-admin.js
@@ -60,12 +60,12 @@ module.exports = function(app){
 	app.get("/sc-admin/workshop/ws/:ws",requireLogin, function(req, res){
 		dbUtil.getAdminWorkshopWithId(req.params.ws, function(data, registered){
 			dbUtil.getQualifications(function(qualificationList){
-				res.render('edititem',
+				res.render('users',
 				{
-					// bannerText:"Edit Workshop",
-					// ws: data,
-					// registered,
-					// qualificationList,
+					bannerText:"Edit Workshop",
+					ws: data,
+					registered,
+					qualificationList,
 				});
 				// res.end();
 			});
