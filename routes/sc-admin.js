eba8c10efc34f6b9c1aa53796cadd259e58f1318
Edwin Wong <edwinwjs94school@gmail.com>
2018-05-31 17:10:15 +0800

    remove compete

--- routes/sc-admin.js
+++ routes/sc-admin.js
@@ -58,21 +58,18 @@ module.exports = function(app){
 	});
 
 	app.get("/sc-admin/workshop/ws/:ws",requireLogin, function(req, res){
-		try{
-			dbUtil.getAdminWorkshopWithId(req.params.ws, function(data, registered){
-				dbUtil.getQualifications(function(qualificationList){
-					res.render('edititem',
-					{
-						bannerText:"Edit Workshop",
-						ws: data,
-						registered,
-						qualificationList,
-					});
-					res.end();
+		// dbUtil.getAdminWorkshopWithId(req.params.ws, function(data, registered){
+			dbUtil.getQualifications(function(qualificationList){
+				res.render('edititem',
+				{
+					bannerText:"Edit Workshop",
+					ws: data,
+					registered,
+					qualificationList,
 				});
+				res.end();
 			});
-		}catch(err){
-		}
+		// });
 	});
 
 	app.get("/sc-admin/create-workshop", requireLogin,function(req, res){
